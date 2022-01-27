efd4221cc635f69878b8b698a0f666382ad3422a
malyshevIY <february14iy@gmail.com>
2022-01-27 19:44:13 +0300

    Добавлено несколько функций (Лекция 1.7))
    Лекция 1.7

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,29 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hellow, world!");
+        hellow("world");
+        hellow("user");
+        hellow("Ilya");
+
+        double l = 5;
+        System.out.println("Площаль квадрата со стороной " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площаль треугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+    }
+
+    public static void hellow(String somebody) {
+        System.out.println("Hellow, " + somebody + "!");
+    }
+
+    public static double area (double len) {
+        return len * len;
+    }
+
+    public static double area (double a, double b) {
+        return a * b;
     }
 
 }
