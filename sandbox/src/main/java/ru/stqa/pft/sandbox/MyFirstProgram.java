8028b20b50edaf4a55e36daed74d88fa9ab26b04
malyshevIY <february14iy@gmail.com>
2022-01-28 01:21:40 +0300

    Использование функций, классов, объектов и методов (Задание №2)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,29 +3,9 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        hellow("world");
-        hellow("user");
-        hellow("Ilya");
 
-        double l = 5;
-        System.out.println("Площаль квадрата со стороной " + l + " = " + area(l));
-
-        double a = 4;
-        double b = 6;
-        System.out.println("Площаль треугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+        Point p = new Point(5,7);
+        System.out.println("Расстояние между точкой p1 и p2 на двумерной плоскости" + " = " + p.distance());
 
     }
-
-    public static void hellow(String somebody) {
-        System.out.println("Hellow, " + somebody + "!");
-    }
-
-    public static double area (double len) {
-        return len * len;
-    }
-
-    public static double area (double a, double b) {
-        return a * b;
-    }
-
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+
+  public double p1;
+  public double p2;
+
+  public Point (double p1, double p2) {
+    this.p1 = p1;
+    this.p2 = p2;
+  }
+
+  public double distance() {
+    return Math.sqrt((this.p1 * this.p1) + (this.p2 * this.p2));
+  }
+
+}
