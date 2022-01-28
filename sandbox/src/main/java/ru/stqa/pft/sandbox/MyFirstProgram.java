f548d92fc8890373bd06c223883775cd4c26b178
malyshevIY <february14iy@gmail.com>
2022-01-28 16:43:04 +0300

    Создание класса, функции и конструктора объектов класса. (задание 1.1, 1.2, 1.3)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,8 +4,14 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
 
-        Point p = new Point(5,7);
-        System.out.println("Расстояние между точкой p1 и p2 на двумерной плоскости" + " = " + p.distance());
+        Point p1 = new Point(7, 8);
+        Point p2 = new Point(10, 15);
+        System.out.println("Расстояние между p1 и p2 на плоскости" + " = " + distance(p1, p2));
 
     }
-}
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    }
+
+    }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,16 +2,12 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-  public double p1;
-  public double p2;
+  public double x;
+  public double y;
 
-  public Point (double p1, double p2) {
-    this.p1 = p1;
-    this.p2 = p2;
-  }
-
-  public double distance() {
-    return Math.sqrt((this.p1 * this.p1) + (this.p2 * this.p2));
+  public Point(double p1, double p2) {
+    this.x = p1;
+    this.y = p2;
   }
 
 }
