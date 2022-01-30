8c6bbce0f1cd628efeb57de69ab33233747cba2a
malyshevIY <february14iy@gmail.com>
2022-01-30 14:11:56 +0300

    Исправлен негативный тест (Задание №3)

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -6,15 +6,15 @@ import org.testng.annotations.Test;
 public class PointTests {
 
   @Test
-  public void testDistance () {
+  public void testDistance() {
     Point a = new Point(7, 8);
     Point b = new Point(10, 15);
     Assert.assertEquals(a.distance(b), 7.615773105863909);
 
   }
 
-@Test
-  public void testDistance1 () {
+  @Test
+  public void testDistance1() {
     Point a = new Point(-7, -8);
     Point b = new Point(-10, -15);
     Assert.assertEquals(a.distance(b), 7.615773105863909);
@@ -22,36 +22,12 @@ public class PointTests {
   }
 
   @Test
-  public void testDistance2 () {
-    Point a = new Point(0, 8);
-    Point b = new Point(10, 15);
-    Assert.assertEquals(a.distance(b), 7.615773105863909);
-
-  }
-
-  @Test
-  public void testDistance3 () {
-    Point a = new Point(0, 8);
-    Point b = new Point(10, 15);
-    Assert.assertEquals(a.distance(b), - 7.615773105863909);
-  }
-
-    @Test
-  public void testDistance4 () {
+  public void testDistance2() {
     Point a = new Point(7, 8);
     Point b = new Point(10, 15);
-    Assert.assertEquals(a.distance(b), 7.666666666666666);
+    Assert.assertNotEquals(a.distance(b), 8);
 
   }
-
-  @Test
-  public void testDistance5 () {
-    Point a = new Point(7, 8);
-    Point b = new Point(10, 15);
-    Assert.assertEquals(a.distance(b), 8.0);
-
-  }
-
 }
 
 
