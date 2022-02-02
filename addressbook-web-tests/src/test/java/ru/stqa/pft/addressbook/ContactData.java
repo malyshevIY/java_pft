3f90c0944e9460f5889d55d9317a78f2fc8c9d91
malyshevIY <february14iy@gmail.com>
2022-02-02 17:17:45 +0300

    fixed contact phone field names (exercise №4)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -1,5 +1,5 @@
 package ru.stqa.pft.addressbook;
 
-public record ContactData(String firstName, String lastName, String adress, String email, String home, String mobile,
-                          String work, String fax) {
+public record ContactData(String firstName, String lastName, String adress, String email, String telephoneHome, String telephoneMobile,
+                          String telephoneWork, String telephoneFax) {
 }
